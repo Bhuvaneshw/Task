0db72b301204d8f1267766ec2dc182d45672ec36
Bhuvaneshwaran <68098126+Bhuvaneshw@users.noreply.github.com>
2023-04-11 11:23:27 +0530

    Update MainActivity.java

--- app/src/main/java/com/acutecoder/taskdemo/MainActivity.java
+++ app/src/main/java/com/acutecoder/taskdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.acutecoder.taskdemo;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textview);
+        //Java 1.7
+        new Task.Java_1_7<>(new Task.Runnable<String>() {
+                    @Override
+                    public String run() throws Exception {
+                        Task.Foreground.run(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Toast in background task", Toast.LENGTH_SHORT).show(); // Toast in UI Thread
+                            }
+                        });
+                        Thread.sleep(1000);
+                        return "Hello";
+                    }
+                })
+                .doInBackground() //or .doInForeground() # default it will be background task
+                .onStart(new Task.Callback() {
+                    @Override
+                    public void callback() {
+                        textView.setText("Starting task");
+                    }
+                })
+                .onEnd(new Task.Callback() {
+                    @Override
+                    public void callback() {
+                        textView.append("\n" + "Task Finished");
+                    }
+                })
+                .onResult(new Task.Result<String>() {
+                    @Override
+                    public void onResult(String result) {
+                        textView.append("\n" + result);
+                    }
+                })
+                .onError(new Task.Error() {
+                    @Override
+                    public void onError(Exception e) {
+                        textView.append("\n" + e.getMessage());
+                    }
+                })
+                .start();
+
+        //Java 1.8
         Task.with(() -> null)
                 .doInBackground() //or .doInForeground() # default it will be background task
                 .onStart(() -> textView.setText("Starting task"))
